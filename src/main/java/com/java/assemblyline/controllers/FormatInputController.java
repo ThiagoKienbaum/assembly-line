971c513c4a314f661e50ea984e3382e2ed77794e
Thiago Kienbaum <thiago.kienbaum@hotmail.com>
2020-08-04 18:41:02 -0300

    feat: Created FormatInputController

--- src/main/java/com/java/assemblyline/controllers/FormatInputController.java
+++ src/main/java/com/java/assemblyline/controllers/FormatInputController.java
@@ -1,4 +1,32 @@
 package com.java.assemblyline.controllers;
 
+import com.java.assemblyline.models.ProcessSchedule;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FormatInputController {
+    public static List<ProcessSchedule> formatInput(String input) {
+        final List<String> inputSteps = new ArrayList<>(getSteps(input));
+        List<ProcessSchedule> processSteps = new ArrayList<>();
+
+        for (String step : inputSteps) {
+            processSteps.add(new ProcessSchedule(getTime(step), step));
+        }
+        return processSteps;
+    }
+
+    private static List<String> getSteps(String input) {
+        String[] processSteps = input.split("\n");
+        return Arrays.asList(processSteps);
+    }
+
+    private static int getTime(String step) {
+        if (step.contains("maintenance")) {
+            return 5;
+        } else {
+            return Integer.valueOf(step.replaceAll("\\D+",""));
+        }
+    }
 }
